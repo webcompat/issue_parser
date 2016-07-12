7e8f69b8e9d7832f247156c0d4492b8891932af1
Karl Dubost <karl+github@la-grange.net>
2016-07-12 13:58:58 +0900

    #6 Cleaning code

--- dump_webcompat_to_db.py
+++ dump_webcompat_to_db.py
@@ -4,21 +4,23 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import scoped_session
-from sqlalchemy.orm import sessionmaker
 from extract_id_title_url import get_webcompat_data
-from db import engine, db_session
+from db import db_session
 import json
 
+DB_PATH = 'sqlite:////tmp/test.db'
 
 Base = declarative_base()
 Base.query = db_session.query_property()
 
-class Issue(Base):
-    __tablename__ = 'webcompat_issues'
 
+class Issue(Base):
+    '''Issue database class.'''
+    __tablename__ = 'webcompat_issues'
     id = Column(String(128), unique=True, primary_key=True)
     summary = Column(String(256))
     url = Column(String(1024))
@@ -30,9 +32,11 @@ class Issue(Base):
         self.url = url
         self.body = body
 
-Base.metadata.create_all(bind=engine)
 
 def main():
+    '''Core program.'''
+    engine = create_engine(DB_PATH, convert_unicode=True)
+    Base.metadata.create_all(bind=engine)
     live = False
     if live:
         data = get_webcompat_data()[1]
@@ -40,10 +44,10 @@ def main():
         f = open('webcompatdata-bzlike.json', 'r')
         data = json.load(f)
         f.close()
-
     # stuff data into database..
     for bug in data['bugs']:
-        db_session.add(Issue(bug['id'], bug['summary'], bug['url'], bug['body']))
+        db_session.add(
+            Issue(bug['id'], bug['summary'], bug['url'], bug['body']))
     db_session.commit()
 
 
